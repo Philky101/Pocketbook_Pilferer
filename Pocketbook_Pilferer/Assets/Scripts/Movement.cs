70e5b750160663719d166fc670f5324a1627cf77
Philky101 <philky101@gmail.com>
2021-10-23 17:54:02 -0400

    Player movement
    Added basic movement for placeholder

--- Pocketbook_Pilferer/Assets/Scripts/Movement.cs
+++ Pocketbook_Pilferer/Assets/Scripts/Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 2DMovement : MonoBehaviour
+public class Movement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Pocketbook_Pilferer/Assets/Scripts/Player_Movement.cs
+++ Pocketbook_Pilferer/Assets/Scripts/Player_Movement.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+	float playerSpeed = 3000f;
+	Rigidbody2D playerBody;
+	
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+	
+	void Move()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		Vector2 movement = new Vector2(inputX, inputY);
+		
+		playerBody.velocity = movement * Time.deltaTime * playerSpeed;
+	}
 }
